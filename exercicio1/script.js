a8f2ab843af6d131022ddb59a90a8cee89e2b56a
danilogmarques <danilogustavomarques@gmail.com>
2022-08-02 16:21:21 -0300

    exercicios a011

---
+++ exercicio1/script.js
@@ -0,0 +1,24 @@
+const sacolao = []
+
+
+const frutasSacolao = [{
+    nome: "banana",
+    preco: 2.50,
+    disponibilidade: true,
+},
+
+{
+    nome: "abacate",
+    preco: 5,
+    disponibilidade: false,
+},
+
+{
+    nome: "manga",
+    preco: 3,
+    disponibilidade: true,
+}]
+console.log(frutasSacolao)
+
+sacolao.push(frutasSacolao);
+console.log(sacolao)
